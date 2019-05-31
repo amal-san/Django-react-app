8623c87abc8b45dcc5cf9b7dee559e80718617d1
Amalsanthoshp <amaldemaniax@gmail.com>
2019-05-31 16:15:31 +0530

    model

--- app/challenges/models.py
+++ app/challenges/models.py
@@ -5,8 +5,18 @@ class Challenge(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     class Meta:
-        verbose_name = "Challenges"
-        verbose_name_plural = "Challenge"
+        verbose_name = "Challenge"
+        verbose_name_plural = "Challenges"
     
     def __str__(self):
         return self.name
+
+class File(models.Model):
+    file = models.FileField(blank=False, null=False)
+ 
+    class Meta:
+        verbose_name="File"
+        verbose_name_plural="Files"
+
+    def __str__(self):
+        return self.file.name

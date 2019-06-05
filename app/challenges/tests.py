75908df83f44d73f03811c45052b00e3eee21719
Amalsanthoshp <amaldemaniax@gmail.com>
2019-06-05 14:09:32 +0530

    test

--- app/challenges/tests.py
+++ app/challenges/tests.py
@@ -13,12 +13,12 @@ class ChallengeModelTest(TestCase):
     def test_title_content(self):
         challenge = Challenge.objects.get(id=1)
         expected_object_name = f'{challenge.name}'
-        self.assertEquals(expected_object_name, 'first name')
+        self.assertEquals(expected_object_name,'first name')
 
     def test_description_content(self):
         challenge = Challenge.objects.get(id=2)
         expected_object_name = f'{challenge.description}'
-        self.assertEquals(expected_object_name, 'a description here')
+        self.assertEquals(expected_object_name,'a description here')
 
 
  

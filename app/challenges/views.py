a4a73cab495dced8a8601356fac8740ecf25c840
Amalsanthoshp <amaldemaniax@gmail.com>
2019-06-05 14:09:01 +0530

    view

--- app/challenges/views.py
+++ app/challenges/views.py
@@ -2,6 +2,11 @@ from rest_framework import generics
 
 from .models import Challenge
 from .serializers import ChallengeSerializer
+from rest_framework.parsers import FileUploadParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
 
 
 class ListChallenge(generics.ListCreateAPIView):
@@ -12,3 +17,4 @@ class ListChallenge(generics.ListCreateAPIView):
 class DetailChallenge(generics.RetrieveUpdateDestroyAPIView):
     queryset = Challenge.objects.all()
     serializer_class = ChallengeSerializer
+

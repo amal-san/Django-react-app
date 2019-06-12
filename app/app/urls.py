12faf8c065abd2e5dd8c431fb0c723590d63328c
Amalsanthoshp <amaldemaniax@gmail.com>
2019-06-12 14:25:22 +0530

    urls.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('challenges.urls')),
+    path('',include('challenges.urls')),
     
 
     

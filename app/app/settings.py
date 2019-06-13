6b848f20be34f71b55ddb72f493545516bc15162
Amalsanthoshp <amaldemaniax@gmail.com>
2019-06-13 11:51:21 +0530

    settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'rest_framework',
     'corsheaders', # new
 
@@ -134,3 +133,10 @@ STATIC_URL = '/static/'
 MEDIA_URL =  '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 APPEND_SLASH=False
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : ('rest_framework.permissions.IsAuthenticated',),
+    'DEFAULT_AUTHENTICATION_CLASSES':  ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+
+}

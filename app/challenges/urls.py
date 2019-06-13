16bba1f27e2fb1a3be4a2d4a1f32cc5834e2fa5a
Amalsanthoshp <amaldemaniax@gmail.com>
2019-06-13 14:25:20 +0530

    urls

--- app/challenges/urls.py
+++ app/challenges/urls.py
@@ -1,14 +1,11 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 from .views import *
-from rest_framework_jwt.views import obtain_jwt_token
-from rest_framework_jwt.views import verify_jwt_token
+from rest_framework import routers
 
-
+router = routers.DefaultRouter()
+router.register('challenges',views.ChallengeView)
 
 urlpatterns = [
-    path('', views.ListChallenge.as_view()),
-    path('<int:pk>/', views.DetailChallenge.as_view()),
-    path('api-token/', obtain_jwt_token),
-    path('api-token-verify/',verify_jwt_token)
+    path('',include(router.urls))
 ]

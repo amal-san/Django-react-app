01243d0eacde6072989363bf14deb5bc70705f3a
Amalsanthoshp <amaldemaniax@gmail.com>
2019-06-13 11:51:53 +0530

    urls

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,10 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('challenges.urls')),
+    path('api-auth/',include('rest_framework.urls')),
+    path('api/token/',TokenObtainPairView.as_view()),
+    path('api/token/refresh/',TokenRefreshView.as_view())
     
 
     

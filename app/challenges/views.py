8e3083cb31dba9cf2819ddab0a72a1c41e6361c3
Amalsanthoshp <amaldemaniax@gmail.com>
2019-06-15 19:00:46 +0530

    views

--- app/challenges/views.py
+++ app/challenges/views.py
@@ -1,20 +1,9 @@
-from rest_framework import generics
-
+from django.shortcuts import render
+from rest_framework import viewsets
 from .models import Challenge
 from .serializers import ChallengeSerializer
-from rest_framework.parsers import FileUploadParser
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status
 
 
-
-class ListChallenge(generics.ListCreateAPIView):
+class ChallengeView(viewsets.ModelViewSet):
     queryset = Challenge.objects.all()
     serializer_class = ChallengeSerializer
-
-
-class DetailChallenge(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Challenge.objects.all()
-    serializer_class = ChallengeSerializer
-
